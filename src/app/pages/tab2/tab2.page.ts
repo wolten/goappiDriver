d95f480392769dd44db8f0c00af5b89ba8a69260
woltenx <wolten@gmail.com>
2020-05-13 17:55:14 -0600

    Agregamos un color rosa y diseñamos el HOME

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { Usuario } from 'src/app/interfaces/interfaces';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
 
-  constructor() {}
+  usuario: Usuario = {};
+  
+  constructor(private usuarioService: UsuarioService) { }
+
+  ngOnInit() {
+
+    this.usuario = this.usuarioService.getUsuario();
+    console.log('TAB2: ', this.usuario);
+  }
 
 }
